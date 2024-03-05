0aa1c3314a65b72be4eb9ad1f9835636cf2c6973
root <root@yq01-sys-hic-k8s-v100-box-a225-0233.yq01.baidu.com>
2024-03-05 20:32:49 +0800

    update

---
+++ run/ernie-pixel-only/ft_ernie-pixel-only_rte.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -e
+
+export PYTHONPATH=$PYTHONPATH:src/
+
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+# Note on GLUE: 
+# We found that for some of the tasks (e.g. MNLI), PIXEL can get stuck in a bad local optimum
+# A clear indicator of this is when the training loss is not decreasing substantially within the first 1k-3k steps
+# If this happens, you can tweak the learning rate slightly, increase the batch size,
+# change rendering backends, or often even just the random seed
+# We are still trying to find the optimal training recipe for PIXEL on these tasks,
+# the recipes used in the paper may not be the best ones out there
+
+# Settings
+NUM_NODE=4
+MASTER_POART=23455
+
+TASK="rte"
+MODEL="pretrained_models/ernie-pixel-only/checkpoint-27500/" # also works with "bert-base-cased", "roberta-base", etc.
+RENDERING_BACKEND="pygame"  # Consider trying out both "pygame" and "pangocairo" to see which one works best
+SEQ_LEN=768
+BSZ=8
+GRAD_ACCUM=None  # We found that higher batch sizes can sometimes make training more stable
+LR=None
+SEED=42
+MAX_STEPS=None
+
+WARMUP_STEPS=10
+EVAL_STEPS=50
+SAVE_STEPS=50
+
+
+
+# === DEBUG ===
+# RUN_NAME=test_preprocess-on-the-fly
+# =============
+
+for LR in 3e-5
+do
+    for GRAD_ACCUM in 1
+    do
+        for MAX_STEPS in 2000
+            do
+                RUN_NAME="ernie-pixel-only-${TASK}-$(basename ${MODEL})-${RENDERING_BACKEND}-${MODALITY}-${SEQ_LEN}-${BSZ}-${GRAD_ACCUM}-${NUM_NODE}-${LR}-${MAX_STEPS}-${SEED}"
+
+                python -m torch.distributed.launch --nproc_per_node=${NUM_NODE} --master_port=${MASTER_POART} scripts/training/run_ernie-pixel_glue.py \
+                --model_name_or_path=${MODEL} \
+                --model_type=ernie-pixel \
+                --processor_name=renderers/noto_renderer \
+                --task_name=${TASK} \
+                --load_from_file=True \
+                --train_file=/root/paddlejob/workspace/env_run/liuqingyi01/pixel_data/${TASK}-train/part-00000.gz \
+                --validation_file=/root/paddlejob/workspace/env_run/liuqingyi01/pixel_data/${TASK}-validation/part-00000.gz \
+                --test_file=/root/paddlejob/workspace/env_run/liuqingyi01/pixel_data/${TASK}-test/part-00000.gz \
+                --rendering_backend=${RENDERING_BACKEND} \
+                --remove_unused_columns=False \
+                --max_steps=${MAX_STEPS} \
+                --do_train \
+                --do_eval \
+                --do_predict \
+                --max_seq_length=${SEQ_LEN} \
+                --early_stopping=False \
+                --warmup_steps=${WARMUP_STEPS} \
+                --per_device_train_batch_size=${BSZ} \
+                --gradient_accumulation_steps=${GRAD_ACCUM} \
+                --learning_rate=${LR} \
+                --run_name=${RUN_NAME} \
+                --output_dir=${RUN_NAME} \
+                --overwrite_output_dir \
+                --overwrite_cache \
+                --logging_strategy=steps \
+                --logging_steps=1 \
+                --evaluation_strategy=steps \
+                --eval_steps=${EVAL_STEPS} \
+                --save_strategy=steps \
+                --save_steps=${SAVE_STEPS} \
+                --save_total_limit=1 \
+                --report_to=tensorboard \
+                --log_predictions \
+                --load_best_model_at_end=True \
+                --seed=${SEED}
+            done
+    done
+done
