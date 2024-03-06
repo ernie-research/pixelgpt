83b320ee57e9a5c4396dda00a32b91a62706d1c9
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-06 17:51:45 +0800

    update

--- run/ernie-clm-base/ft_ernie-clm-base_qnli.sh
+++ run/ernie-clm-base/ft_ernie-clm-base_qnli.sh
@@ -21,7 +21,7 @@ MASTER_POART=23456
 MODALITY="text"
 
 TASK="qnli"
-MODEL="pretrained_models/ernie-pixel-mono/checkpoint-13750/" # also works with "bert-base-cased", "roberta-base", etc.
+MODEL="pretrained_models/ernie-clm-base/checkpoint-27500/" # also works with "bert-base-cased", "roberta-base", etc.
 RENDERING_BACKEND="pygame"  # Consider trying out both "pygame" and "pangocairo" to see which one works best
 SEQ_LEN=768
 BSZ=8
