a5b545b99723dbddcfae6efce4ac091d1b174eac
root <root@yq01-sys-hic-k8s-v100-box-a225-0259.yq01.baidu.com>
2024-03-07 13:32:53 +0800

    update

--- run/ernie-pixel-only/ft_ernie-pixel-only_stsb.sh
+++ run/ernie-pixel-only/ft_ernie-pixel-only_stsb.sh
@@ -4,8 +4,6 @@ set -e
 
 export PYTHONPATH=$PYTHONPATH:src/
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-
 # Note on GLUE: 
 # We found that for some of the tasks (e.g. MNLI), PIXEL can get stuck in a bad local optimum
 # A clear indicator of this is when the training loss is not decreasing substantially within the first 1k-3k steps
@@ -14,13 +12,11 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 # We are still trying to find the optimal training recipe for PIXEL on these tasks,
 # the recipes used in the paper may not be the best ones out there
 
-# =====================Settings========================
-NUM_NODE=4
-MASTER_POART=23458
+# Settings
+NUM_NODE=8
+MASTER_POART=23457
 
-MODALITY="image"
-
-TASK="wnli"
+TASK="stsb"
 MODEL="pretrained_models/ernie-pixel-only/checkpoint-25000/" # also works with "bert-base-cased", "roberta-base", etc.
 RENDERING_BACKEND="pygame"  # Consider trying out both "pygame" and "pangocairo" to see which one works best
 SEQ_LEN=768
@@ -31,33 +27,27 @@ SEED=42
 MAX_STEPS=None
 
 WARMUP_STEPS=10
-EVAL_STEPS=5
-SAVE_STEPS=5
+EVAL_STEPS=50
+SAVE_STEPS=50
 
-# early stopping
-IS_EARLY_STOPPING=True
-METRIC_FOR_BEST_MODEL="eval_accuracy"
-EARLY_STOPPING_PATIENCE=8
-GREATER_IS_BETTER=True
 
 
 # === DEBUG ===
 # RUN_NAME=test_preprocess-on-the-fly
 # =============
 
-for LR in 1e-5 3e-5 5e-5
+for LR in 5e-5
 do
-    for GRAD_ACCUM in 1 2 4 8
+    for GRAD_ACCUM in 8
     do
-        for MAX_STEPS in 250
+        for MAX_STEPS in 500
             do
                 RUN_NAME="ernie-pixel-only/${TASK}-$(basename ${MODEL})-${RENDERING_BACKEND}-${MODALITY}-${SEQ_LEN}-${BSZ}-${GRAD_ACCUM}-${NUM_NODE}-${LR}-${MAX_STEPS}-${SEED}"
 
-                python -m torch.distributed.launch --nproc_per_node=${NUM_NODE} --master_port=${MASTER_POART} scripts/training/run_ernie-pixel_glue.py \
+                python -m torch.distributed.launch --nproc_per_node=8 scripts/training/run_ernie-pixel_glue.py \
                 --model_name_or_path=${MODEL} \
                 --model_type=ernie-pixel \
                 --processor_name=renderers/noto_renderer \
-                --modality=${MODALITY} \
                 --task_name=${TASK} \
                 --load_from_file=True \
                 --train_file=/root/paddlejob/workspace/env_run/liuqingyi01/pixel_data/${TASK}-train/part-00000.gz \
@@ -86,12 +76,8 @@ do
                 --save_strategy=steps \
                 --save_steps=${SAVE_STEPS} \
                 --save_total_limit=1 \
-                --metric_for_best_model=${METRIC_FOR_BEST_MODEL} \
                 --report_to=tensorboard \
                 --log_predictions \
-                --early_stopping=${IS_EARLY_STOPPING} \
-                --early_stopping_patience=${EARLY_STOPPING_PATIENCE} \
-                --greater_is_better=${GREATER_IS_BETTER} \
                 --load_best_model_at_end=True \
                 --seed=${SEED}
             done
