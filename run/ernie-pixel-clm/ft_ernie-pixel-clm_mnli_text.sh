7135e2b1c15a471114c5ab7aee4ac72e95816d92
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-08 11:33:51 +0800

    update

--- run/ernie-pixel-clm/ft_ernie-pixel-clm_mnli_text.sh
+++ run/ernie-pixel-clm/ft_ernie-pixel-clm_mnli_text.sh
@@ -54,7 +54,7 @@ do
     do
         for MAX_STEPS in 8000
             do
-                RUN_NAME="ernie-pixel-clm-${TASK}-$(basename ${MODEL})-${RENDERING_BACKEND}-${MODALITY}-${SEQ_LEN}-${BSZ}-${GRAD_ACCUM}-${NUM_NODE}-${LR}-${MAX_STEPS}-${SEED}"
+                RUN_NAME="ernie-pixel-clm/${TASK}-$(basename ${MODEL})-${RENDERING_BACKEND}-${MODALITY}-${SEQ_LEN}-${BSZ}-${GRAD_ACCUM}-${NUM_NODE}-${LR}-${MAX_STEPS}-${SEED}"
 
                 python -m torch.distributed.launch --nproc_per_node=${NUM_NODE} --master_port=${MASTER_POART} scripts/training/run_ernie-pixel_glue.py \
                 --model_name_or_path=${MODEL} \
