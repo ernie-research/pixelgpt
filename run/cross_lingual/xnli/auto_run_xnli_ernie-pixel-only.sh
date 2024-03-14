87a10e2684625660f9d2dd8c41d9c1f86118a747
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-14 22:02:54 +0800

    update

---
+++ run/cross_lingual/xnli/auto_run_xnli_ernie-pixel-only.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+export PYTHONPATH=$PYTHONPATH:src/
+
+# ==== 设置ckpt step ====
+step=12500 # 指定你的ckpt步数
+PT_TYPE=ernie-pixel-only # 指定你的预训练类型
+# =======================
+
+
+# pretrained model path
+MODEL=pretrained_models/$PT_TYPE/checkpoint-${step}/
+
+CKPT_NAME=ckpt-${step}
+
+# ===== train-all; ft image =====
+LOG_DIR=log/cross_lingual/xnli/train_all/$PT_TYPE/$CKPT_NAME
+mkdir -p $LOG_DIR
+bash run/cross_lingual/xnli/train_all/$PT_TYPE/ft_ernie-pixel_mono_xnli_image.sh $MODEL > $LOG_DIR/${CKPT_NAME}_image.log 2>&1
+sleep 60
+
+# ===== train-en; ft image =====
+LOG_DIR=log/cross_lingual/xnli/train_en/$PT_TYPE/$CKPT_NAME
+mkdir -p $LOG_DIR
+bash run/cross_lingual/xnli/train_en/$PT_TYPE/ft_ernie-pixel_mono_xnli_image.sh $MODEL > $LOG_DIR/${CKPT_NAME}_image.log 2>&1
+sleep 60
+
+
+echo "finished!"
