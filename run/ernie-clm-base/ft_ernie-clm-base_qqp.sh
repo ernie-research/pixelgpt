d936ba4e8e406a48619223ee98fd3713c88dd919
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-14 15:06:08 +0800

    update

--- run/ernie-clm-base/ft_ernie-clm-base_qqp.sh
+++ run/ernie-clm-base/ft_ernie-clm-base_qqp.sh
@@ -51,7 +51,7 @@ do
     do
         for MAX_STEPS in 15000
             do
-                RUN_NAME="ernie-clm-base/${TASK}-$(basename ${MODEL})/${TASK}-$(basename ${MODEL})-${RENDERING_BACKEND}-${MODALITY}-${SEQ_LEN}-${BSZ}-${GRAD_ACCUM}-${NUM_NODE}-${LR}-${MAX_STEPS}-${SEED}"
+                RUN_NAME="ernie-clm-base/$(basename ${MODEL})/${TASK}-$(basename ${MODEL})-${RENDERING_BACKEND}-${MODALITY}-${SEQ_LEN}-${BSZ}-${GRAD_ACCUM}-${NUM_NODE}-${LR}-${MAX_STEPS}-${SEED}"
 
                 python -m torch.distributed.launch --nproc_per_node=${NUM_NODE} --master_port=${MASTER_POART} scripts/training/run_ernie-pixel_glue.py \
                 --model_name_or_path=${MODEL} \
@@ -91,7 +91,7 @@ do
                 --early_stopping_patience=${EARLY_STOPPING_PATIENCE} \
                 --greater_is_better=${GREATER_IS_BETTER} \
                 --load_best_model_at_end=True \
-                --bf16 \
+                --fp16 \
                 --seed=${SEED}
             done
     done
