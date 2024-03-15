b2ceac0beeeef84b35c31f399f05b5d2c580ac0a
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-15 12:21:06 +0800

    udpate

--- run/cross_lingual/xnli/train_en/ernie-pixel-clm/ft_ernie-pixel-clm_xnli_image.sh
+++ run/cross_lingual/xnli/train_en/ernie-pixel-clm/ft_ernie-pixel-clm_xnli_image.sh
@@ -92,9 +92,9 @@ do
                 --load_best_model_at_end=True \
                 --seed=${SEED} \
                 --fp16
+
+                # 格式化结果
+                python src/utils/format_result_xnli.py $RUN_NAME
             done
     done
 done
-
-# 格式化结果
-python src/utils/format_result_xnli.py $RUN_NAME
