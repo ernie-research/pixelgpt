84e64031f9c55f4b12f075904d2a15e54ac50e5b
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-24 10:40:03 +0800

    udpate

--- run/cross_lingual/xnli/auto_run_xnli_ernie-pixel-clm.sh
+++ run/cross_lingual/xnli/auto_run_xnli_ernie-pixel-clm.sh
@@ -5,7 +5,7 @@ set -e
 export PYTHONPATH=$PYTHONPATH:src/
 
 # ==== 设置ckpt step ====
-step=12500 # 指定你的ckpt步数
+step=4000 # 指定你的ckpt步数
 PT_TYPE=ernie-pixel-clm # 指定你的预训练类型
 # =======================
 
@@ -16,10 +16,10 @@ MODEL=pretrained_models/$PT_TYPE/checkpoint-${step}/
 CKPT_NAME=ckpt-${step}
 
 # ===== train-all; ft text =====
-# LOG_DIR=log/cross_lingual/xnli/train_all/$PT_TYPE/$CKPT_NAME
-# mkdir -p $LOG_DIR
-# bash run/cross_lingual/xnli/train_all/$PT_TYPE/ft_${PT_TYPE}_xnli_text.sh $MODEL > $LOG_DIR/${CKPT_NAME}_text.log 2>&1
-# sleep 60
+LOG_DIR=log/cross_lingual/xnli/train_all/$PT_TYPE/$CKPT_NAME
+mkdir -p $LOG_DIR
+bash run/cross_lingual/xnli/train_all/$PT_TYPE/ft_${PT_TYPE}_xnli_text.sh $MODEL > $LOG_DIR/${CKPT_NAME}_text.log 2>&1
+sleep 60
 
 # ===== train-en; ft text =====
 LOG_DIR=log/cross_lingual/xnli/train_en/$PT_TYPE/$CKPT_NAME
--- run/cross_lingual/xnli/auto_run_xnli_ernie-pixel-mono.sh
+++ run/cross_lingual/xnli/auto_run_xnli_ernie-pixel-mono.sh
@@ -4,6 +4,9 @@ set -e
 
 export PYTHONPATH=$PYTHONPATH:src/
 
+echo "sleeping..."
+sleep 1800
+
 # ==== 设置ckpt step ====
 step=12500 # 指定你的ckpt步数
 PT_TYPE=ernie-pixel-mono # 指定你的预训练类型
@@ -22,10 +25,10 @@ CKPT_NAME=ckpt-${step}
 # sleep 60
 
 # ===== train-en; ft text =====
-LOG_DIR=log/cross_lingual/xnli/train_en/$PT_TYPE/$CKPT_NAME
-mkdir -p $LOG_DIR
-bash run/cross_lingual/xnli/train_en/$PT_TYPE/ft_${PT_TYPE}_xnli_text.sh $MODEL > $LOG_DIR/${CKPT_NAME}_text.log 2>&1
-sleep 60
+# LOG_DIR=log/cross_lingual/xnli/train_en/$PT_TYPE/$CKPT_NAME
+# mkdir -p $LOG_DIR
+# bash run/cross_lingual/xnli/train_en/$PT_TYPE/ft_${PT_TYPE}_xnli_text.sh $MODEL > $LOG_DIR/${CKPT_NAME}_text.log 2>&1
+# sleep 60
 
 # ===== train-all; ft image =====
 LOG_DIR=log/cross_lingual/xnli/train_all/$PT_TYPE/$CKPT_NAME
@@ -34,10 +37,10 @@ bash run/cross_lingual/xnli/train_all/$PT_TYPE/ft_${PT_TYPE}_xnli_image.sh $MODE
 sleep 60
 
 # ===== train-en; ft image =====
-LOG_DIR=log/cross_lingual/xnli/train_en/$PT_TYPE/$CKPT_NAME
-mkdir -p $LOG_DIR
-bash run/cross_lingual/xnli/train_en/$PT_TYPE/ft_${PT_TYPE}_xnli_image.sh $MODEL > $LOG_DIR/${CKPT_NAME}_image.log 2>&1
-sleep 60
+# LOG_DIR=log/cross_lingual/xnli/train_en/$PT_TYPE/$CKPT_NAME
+# mkdir -p $LOG_DIR
+# bash run/cross_lingual/xnli/train_en/$PT_TYPE/ft_${PT_TYPE}_xnli_image.sh $MODEL > $LOG_DIR/${CKPT_NAME}_image.log 2>&1
+# sleep 60
 
 
 echo "finished!"
