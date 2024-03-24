1406d7906c33712746b8184ed8f15b1461d21be2
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-24 10:07:32 +0800

    udpate

--- auto_run_ernie-pixel-clm_pair.sh
+++ auto_run_ernie-pixel-clm_pair.sh
@@ -13,10 +13,11 @@ LOG_DIR=log/ft_ernie-pixel-clm/$CKPT_NAME
 
 mkdir -p $LOG_DIR
 
-for TASK in mnli qqp qnli sst2 cola mrpc stsb rte wnli
+# for TASK in mnli qqp qnli sst2 cola mrpc stsb rte wnli
+for TASK in mnli qqp
 do
     echo "running ${TASK}..."
-    bash run/ernie-pixel-clm/ft_ernie-pixel-clm_${TASK}_pixel.sh $MODEL > $LOG_DIR/ft_ernie-pixel-clm_${TASK}_pixel_${CKPT_NAME}.log 2>&1
+    bash run/ernie-pixel-clm/ft_ernie-pixel-clm_${TASK}_pair.sh $MODEL > $LOG_DIR/ft_ernie-pixel-clm_${TASK}_pair_${CKPT_NAME}.log 2>&1
 done
 
 echo "finished!"
