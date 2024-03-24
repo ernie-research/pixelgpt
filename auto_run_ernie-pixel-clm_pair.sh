0fa30aaee89880e421020966520b98622ff9e2ab
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-24 09:38:39 +0800

    update

--- auto_run_ernie-pixel-clm_pair.sh
+++ auto_run_ernie-pixel-clm_pair.sh
@@ -5,7 +5,7 @@ set -e
 export PYTHONPATH=$PYTHONPATH:src/
 
 # ==== 设置ckpt step ====
-step=5000
+step=9625
 
 MODEL=pretrained_models/ernie-pixel-clm/checkpoint-${step}/
 CKPT_NAME=ckpt-${step}
@@ -13,12 +13,10 @@ LOG_DIR=log/ft_ernie-pixel-clm/$CKPT_NAME
 
 mkdir -p $LOG_DIR
 
-# for TASK in mnli qqp qnli sst2 cola mrpc stsb rte wnli
-for TASK in mnli
+for TASK in mnli qqp qnli sst2 cola mrpc stsb rte wnli
 do
     echo "running ${TASK}..."
-    # bash run/ernie-pixel-clm/ft_ernie-pixel-clm_${TASK}_pixel.sh $MODEL > $LOG_DIR/ft_ernie-pixel-clm_${TASK}_pixel_${CKPT_NAME}.log 2>&1
-    bash run/ernie-pixel-clm/ft_ernie-pixel-clm_${TASK}_pair.sh $MODEL > $LOG_DIR/ft_ernie-pixel-clm_${TASK}_pair_${CKPT_NAME}_grid-search.log 2>&1
+    bash run/ernie-pixel-clm/ft_ernie-pixel-clm_${TASK}_pixel.sh $MODEL > $LOG_DIR/ft_ernie-pixel-clm_${TASK}_pixel_${CKPT_NAME}.log 2>&1
 done
 
 echo "finished!"
