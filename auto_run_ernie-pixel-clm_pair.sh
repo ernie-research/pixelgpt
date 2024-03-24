ccf086cdb62e07cdc7d971a31ba3fa2c0f566ae9
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-24 09:34:49 +0800

    update

---
+++ auto_run_ernie-pixel-clm_pair.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+export PYTHONPATH=$PYTHONPATH:src/
+
+# ==== 设置ckpt step ====
+step=5000
+
+MODEL=pretrained_models/ernie-pixel-clm/checkpoint-${step}/
+CKPT_NAME=ckpt-${step}
+LOG_DIR=log/ft_ernie-pixel-clm/$CKPT_NAME
+
+mkdir -p $LOG_DIR
+
+# for TASK in mnli qqp qnli sst2 cola mrpc stsb rte wnli
+for TASK in mnli
+do
+    echo "running ${TASK}..."
+    # bash run/ernie-pixel-clm/ft_ernie-pixel-clm_${TASK}_pixel.sh $MODEL > $LOG_DIR/ft_ernie-pixel-clm_${TASK}_pixel_${CKPT_NAME}.log 2>&1
+    bash run/ernie-pixel-clm/ft_ernie-pixel-clm_${TASK}_pair.sh $MODEL > $LOG_DIR/ft_ernie-pixel-clm_${TASK}_pair_${CKPT_NAME}_grid-search.log 2>&1
+done
+
+echo "finished!"
