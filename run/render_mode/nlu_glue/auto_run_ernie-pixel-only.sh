628c1f19b6410beb7ac01990b5ddd5fafcd4d4c5
liuqingyi01 <liuqingyi01@baidu.com>
2024-03-28 22:29:29 +0800

    update

--- run/render_mode/nlu_glue/auto_run_ernie-pixel-only.sh
+++ run/render_mode/nlu_glue/auto_run_ernie-pixel-only.sh
@@ -17,7 +17,7 @@ LOG_DIR=log/ft_ernie-pixel-only/$CKPT_NAME
 mkdir -p $LOG_DIR
 
 # for TASK in mnli qqp qnli sst2 cola mrpc stsb rte wnli
-for TASK in mnli qqp qnli sst2 cola mrpc stsb rte wnli
+for TASK in mnli
 do
     echo "running ${TASK}..."
     bash run/render_mode/nlu_glue/ernie-pixel-only/ft_ernie-pixel-only_${TASK}.sh $MODEL $RENDER_MODE > $LOG_DIR/ft_ernie-pixel-only_${TASK}_${CKPT_NAME}.log 2>&1
